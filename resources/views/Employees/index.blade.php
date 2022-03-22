7029a5f42b8c46726a820087d326d2f9427bf5f0
anasyoussi <18307320+anasyoussi@users.noreply.github.com>
2022-03-22 22:41:52 +0100

    Project without login

--- resources/views/Employees/index.blade.php
+++ resources/views/Employees/index.blade.php
@@ -22,7 +22,7 @@
         <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-md sm:rounded-lg">
                 <!-- -->
-                <div class="p-4">
+                <!-- <div class="p-4">
                 <label for="table-search" class="sr-only">Search</label>
                 <div class="relative my-1">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -34,7 +34,7 @@
                         </svg>
                     </div>
                     <input type="text" id="table-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
-                </div>
+                </div> -->
                 <!-- -->
                 <table class="min-w-full">
                     <thead class="bg-gray-50 dark:bg-gray-700">
